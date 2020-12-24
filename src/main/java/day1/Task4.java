4086e742d63a79fafbd6a1c68eb6d44b956bbaa4
Aset91 <asetka91@mail.ru>
2020-12-24 22:26:22 +0300

    Day1.4

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task4 {
     public static void main(String[] args) {
+        int year = 1980;
+        for ( int x = year; x <= 2020; x+= 4 ) {
+            System.out.println("Олимпиада " + x + " года");
+        }
 
     }
 }
