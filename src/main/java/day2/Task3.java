95f017ffdffbe4b191bedfc233ea76a7b98dcd79
Aset91 <asetka91@mail.ru>
2020-12-26 20:09:55 +0300

    solution day2 (-)

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -3,5 +3,5 @@ package day2;
 public class Task3 {
     public static void main(String[] args) {
 
-    }
+     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -3,5 +3,5 @@ package day2;
 public class Task4 {
     public static void main(String[] args) {
 
-    }
+     }
 }
