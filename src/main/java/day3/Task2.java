5a591dc3520916ef90ab58572e1333bcc04ad26a
Aset91 <asetka91@mail.ru>
2020-12-29 10:53:06 +0300

    Day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.*;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            if (y == 0) {
+                break;
+            } else {
+                System.out.print(x / y);
+                break;
+            }
+
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.*;
+
 public class Task3 {
     public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Scanner scanner = new Scanner(System.in);
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            System.out.println(x / y);
 
+            if (y == 0) {
+                System.out.println("Деление на 0");
+            }
+        }
     }
 }
