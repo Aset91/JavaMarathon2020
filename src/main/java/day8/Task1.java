45976bee1cf5ded1d653f096cdefc8a51e3b929f
Aset91 <asetka91@mail.ru>
2021-01-05 01:04:19 +0300

    Day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,16 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String x = " ";
+        for (int i = 0; i <= 2000; i++) {
+            System.out.print(i + x);
+        }
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i <= 2000; i++) {
+            b.append(i).append(" ");
+        }
+        System.out.println();
+        System.out.print(b.toString());
 
     }
 }
