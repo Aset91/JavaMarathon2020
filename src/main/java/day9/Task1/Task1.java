d084e449d0c08f7598669537b8cc194c8884b5cf
Aset91 <asetka91@mail.ru>
2021-01-06 21:29:30 +0300

    Day9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,9 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Student student = new Student("Adam", "A");
+        Teacher teacher = new Teacher ("Sergey", "Biology");
+        student.printInfo();
+        teacher.printInfo();
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,37 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
-
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
     }
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+        double redPerimeter = 0;
+        for (Figure f : figures) {
+            if (f.getColor().equals("Red")) {
+                redPerimeter += f.perimeter();
+            }
+        }
+        return redPerimeter;
+    }
+
+    public static double calculateRedArea(Figure[] figures) {
+        double redArea = 0;
+        for (Figure f : figures) {
+            if (f.getColor().equals("Red")) {
+                redArea += f.area();
+            }
+        }
+        return redArea;
+    }
+
 }
+
