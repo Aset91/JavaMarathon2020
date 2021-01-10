6cb451a11b7597b8a5d06c3bac71e6716ba67a00
Aset91 <asetka91@mail.ru>
2021-01-10 13:59:47 +0300

    Day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,33 @@
 package day15;
 
+import java.io.*;
+import java.util.*;
+
 public class Task1 {
+
     public static void main(String[] args) {
+
+        List<String> shoesList = new ArrayList<>();
+        try {
+            File firstFile = new File("src/main/resources/shoes.csv");
+            File secondFile = new File("src/main/resources/missing_shoes");
+            Scanner scanner = new Scanner(firstFile);
+            PrintWriter writer = new PrintWriter(secondFile);
+
+            while (scanner.hasNextLine()) {
+                String str = scanner.nextLine();
+                String[] splitedList = str.split(";");
+                if (Integer.parseInt(splitedList[2]) == 0) {
+                    writer.println(splitedList[0] + ", " + splitedList[1] + ", " + splitedList[2]);
+                }
+
+            }
+            scanner.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
 
     }
 }
