2b7e26fef9dc1a41d27a8006bdffaf5588bcfbc5
Aset91 <asetka91@mail.ru>
2021-01-17 19:41:50 +0300

    Day18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,15 @@
 package day18;
 
 public class Task1 {
-    public static void main(String[] args) {
+    public static int recursionSum(int[] n, int x) {
 
+        if (x == n.length) {
+            return 0; }
+        return n [x] + recursionSum(n, x + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers, 0));
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,20 @@
 package day18;
 
 public class Task2 {
+
+    public static int count7(int digit) {
+        if (digit == 0) {
+            return 0;
+        }
+        if (digit % 10 == 7) {
+            return 1 + count7(digit / 10);
+        } else {
+            return count7(digit / 10);
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.print(count7(7787577));
 
     }
 }
